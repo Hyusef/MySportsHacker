067248d7a9782fe44820cc07649733119b144110
hy <mieyle@protonmail.com>
2023-06-12 18:01:29 +0100

    Get rankings and display in tables

--- source/SportsViews/MMA/MmaSelector.js
+++ source/SportsViews/MMA/MmaSelector.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import SelectInput from 'ink-select-input';
 import UFC from './UFC/index.js';
 import Bellator from './Bellator/index.js';
 import OneChampionship from './OneChampionship/index.js';
-import { useInput } from 'ink';
+import { useInput, render } from 'ink';
 import { MMAOrgs } from '../../Data/SelectorData.js';
+
+
 
 export default function MmaSelector() {
     const [selectedOrg, setSelectedOrg] = React.useState(null);
     const handleSelect = item => { setSelectedOrg(item.value) };
-
     useInput((_, key) => {
-        if (key.leftArrow) {
+        if (key.leftArrow && leftUsed) {
+            console.log('first')
             setSelectedOrg(null);
-
         }
+
     });
     return (
         <>
@@ -27,7 +29,6 @@ export default function MmaSelector() {
 
         </>
     );
-
 };
 
 
