d3d6aadf494aa17e83cb37da201d42fae24524ec
hy <mieyle@protonmail.com>
2023-06-12 19:51:46 +0100

    Add component that gets events

--- source/SportsViews/MMA/UFC/index.js
+++ source/SportsViews/MMA/UFC/index.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Text, Box, render } from 'ink';
+import { Text, Box, render, Newline } from 'ink';
 import * as cheerio from 'cheerio';
 import axios from 'axios';
 import Spinner from 'ink-spinner';
 import { Table } from '@alcalzone/ink-table'
+
 
 const getFighters = async () => {
     const f = []
@@ -79,7 +80,28 @@ const getFighters = async () => {
     return f;
 }
 
-
+const getEvents = async () => {
+    const eventData = [];
+    axios.get("https://www.ufc.com/events").then((resp) => {
+        const data = resp.data;
+        const $ = cheerio.load(data);
+        $('div.view.view-events-upcoming-past-solr.view-id-events_upcoming_past_solr.view-display-id-upcoming').find("article.c-card-event--result").each((_, ele) => {
+            const headline = $(ele).find('h3.c-card-event--result__headline > a ').text()
+            const date = $(ele).find('div.c-card-event--result__date > a').text();
+            const venue = $(ele).find('div.field.field--name-taxonomy-term-title > h5').text().replace(/[\n]/g, "").trim();
+            const city = $(ele).find('p.address > span.locality').text()
+            const country = $(ele).find('p.address > span.country').text()
+            eventData.push({
+                headline: headline,
+                date: date,
+                venue: venue,
+                city: city,
+                country: country,
+            })
+        })
+    })
+    return eventData;
+}
 
 const Spinners = () => {
     return (
@@ -94,15 +116,62 @@ const Spinners = () => {
     )
 }
 
+
+
 export default function UFC() {
 
-
-
-
-
-
-
 }
+
+
+function UfcEvents() {
+    const [events, setEvents] = useState([])
+    const [rerender, setRerender] = useState(false)
+
+    useEffect(() => {
+        const getEventData = async () => {
+            try {
+                const ufcEvents = await getEvents();
+                setEvents(ufcEvents);
+            } catch (error) {
+                console.error("Error fetching events:", error);
+            }
+        };
+        getEventData();
+        setTimeout(() => {
+            setRerender(true)
+        }, 1000)
+    }, []);
+
+    return (
+        <>
+            {
+                events.length == 0 ?
+                    <Spinners />
+                    : (events.map((ele, i) => {
+                        console.log(ele.headline)
+                        return (
+                            <Box borderStyle="round" marginRight={2} borderColor="yellow" >
+                                <Text>
+                                    <Text key={ele.headline}>{ele.headline}</Text>
+                                    <Newline />
+                                    <Text key={ele.city}>{ele.city}</Text>
+                                    <Newline />
+                                    <Text key={i}>{ele.country}</Text>
+                                    <Newline />
+                                    <Text key={i}>{ele.date}</Text>
+                                    <Newline />
+                                    <Text key={ele.venue + i}>{ele.venue}</Text>
+                                </Text>
+
+                            </Box>
+                        )
+                    }))
+            }
+        </>
+    )
+}
+
+
 
 function UfcRankings() {
     const [div, setDiv] = useState([])
@@ -113,7 +182,6 @@ function UfcRankings() {
             try {
                 const fighters = await getFighters();
                 setDiv(fighters);
-                console.log(div);
             } catch (error) {
                 console.error("Error fetching fighters:", error);
             }
