9fef9841b44e18a168b0a241b7f0a32a48255121
hy <mieyle@protonmail.com>
2023-06-13 15:53:43 +0100

    Add BellatorRankings

--- source/SportsViews/MMA/Bellator/index.js
+++ source/SportsViews/MMA/Bellator/index.js
@@ -19,6 +19,25 @@ const Spinners = () => {
     )
 }
 
+const getBellatorRankings = async () => {
+    const ranksData = [];
+    axios.get("https://www.cbssports.com/mma/rankings/bellator/").then((resp) => {
+        const data = resp.data;
+        const $ = cheerio.load(data);
+        $('div.RankingsTablesContainer.SectionLayout--2col').find("div.TableBase.TableBase-RankingsTable").each((i, e) => {
+            const divisionName = $(e).find('h4.TableBase-title.TableBase-title--large').text().replace(/\n/g, "").trim();
+            const division = [];
+            $(e).find('tr.TableBase-bodyTr').each((ind, ele) => {
+                const rank = $(ele).find("td:first").text().replace(/\n/g, "").trim();
+                const fighter = $(ele).find("td:nth-child(2)").text().replace(/\n/g, "").trim();
+                division.push({ rank: rank, fighter: fighter })
+            })
+            ranksData.push({ divisionName: divisionName, division: division });
+        })
+    })
+    return ranksData
+
+}
 
 const getBellatorEvents = async () => {
     const eventData = [];
@@ -41,12 +60,53 @@ const getBellatorEvents = async () => {
 
 
 export default function Bellator() {
+
     return (
         <>
-            <Text>This is Belllator</Text>
+            <Text>Belatorr</Text>
         </>
     )
 }
+
+function BellatorRankings() {
+    const [ranks, setRanks] = useState([])
+    const [rerender, setRerender] = useState(false)
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const bellatorRanks = await getBellatorRankings();
+                setRanks(bellatorRanks);
+            } catch (error) {
+            }
+        };
+        getData();
+        console.log(ranks);
+        setTimeout(() => {
+            setRerender(true)
+        }, 2000)
+    }, []);
+
+
+    return (
+        <>
+            {ranks.length == 0
+                ? <Spinners />
+                : (
+                    ranks.map((e, i) => {
+                        return (
+                            <>
+                                <Text>{e.divisionName}</Text>
+                                <Table data={e.division} />
+                            </>
+                        )
+                    })
+
+                )}
+
+        </>
+    )
+}
+
 
 
 
