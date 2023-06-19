7bbb57fe4f9613e11faa37ceb7bef496278d5453
hy <mieyle@protonmail.com>
2023-06-19 15:49:46 +0100

    Render table with fixtures

--- source/SportsViews/Football/index.js
+++ source/SportsViews/Football/index.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import { Text } from 'ink';
-
+import React, { useState, useEffect } from 'react';
+import { Text, Box, render, Newline } from 'ink';
+import * as cheerio from 'cheerio';
+import axios from 'axios';
+import Spinner from 'ink-spinner';
+import { Table } from '@alcalzone/ink-table'
+const Spinners = () => {
+    return (
+        <>
+            <Text>
+                <Text color="red">
+                    <Spinner type="dots" />
+                </Text>
+                {' Loading'}
+            </Text>
+        </>
+    )
+}
 
 const getTodaysFixtures = async () => {
+    const tournaments = [];
     axios.get("https://www.sportinglife.com/football/fixtures-results").then((resp) => {
         const data = resp.data;
         const $ = cheerio.load(data);
-        const tournaments = [];
         $('ul.CompetitionList__CompetitionListWrapper-sc-1f2woz6-0.PJXXy').find("li.CompetitionList__CompetitionListItem-sc-1f2woz6-1.eqnfuA").each((_, e) => {
             const titleOfCompetition = $(e).find('h3.ContentPanel__ContentPanelTitle-sc-1izwmji-1.kWcBOf').text();
             $(e).find('section.ContentPanel__ContentPanelWrapper-sc-1izwmji-0.cpIOrT').each((_, box) => {
@@ -17,7 +32,7 @@ const getTodaysFixtures = async () => {
                     const teamA = $(match).find('span.Item__TeamA-et8305-6.bZRnjo').text();
                     const teamB = $(match).find('span.Item__TeamB-et8305-8.dqXJJw').text();
                     const teamMatch = teamA + ' V ' + teamB
-                    matches.push({ match: teamMatch, time: time });
+                    matches.push({ Match: teamMatch, Time: time });
                 })
                 tournaments.push(matches)
             })
@@ -28,8 +43,39 @@ const getTodaysFixtures = async () => {
 
 
 export default function Football() {
+    const [fixtures, setFixtures] = useState([])
+    const [rerender, setRerender] = useState(false)
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const fixturesData = await getTodaysFixtures();
+                setFixtures(fixturesData);
+            } catch (error) {
+            }
+        };
+        getData();
+        setTimeout(() => {
+            setRerender(true)
+        }, 2000)
+    }, []);
+
     return (
-        <Text>Football</Text>
+        <>
+            {fixtures?.length == 0
+                ? <Spinners />
+                : (
+                    fixtures?.map((e, i) => {
+                        return (
+                            <>
+                                <Table data={e.slice(1)} ></Table>
+                            </>
+                        )
+                    })
+
+                )}
+
+        </>
     )
 
 }
