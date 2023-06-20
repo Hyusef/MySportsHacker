b4ca137e8ecbc53bde1843376a9c45b1d949d0e4
hy <mieyle@protonmail.com>
2023-06-20 11:46:39 +0100

    Change function to fetch both tables

--- source/SportsViews/NBA/index.js
+++ source/SportsViews/NBA/index.js
@@ -20,12 +20,13 @@ const Spinners = () => {
 
 //create a way of getting input from user and include an option if they want to see both eastern and western standings,
 //if its only one then edit the function below to take a parameter which changes which table is scraped
-const getEasternRankings = async () => {
-    const easternRankings = [];
+const getEasternRankings = async (rank) => {
+    const rankings = [];
+    const tableSelect = `div#TableBase-${rank}`
     axios.get("https://www.cbssports.com/nba/standings/").then((resp) => {
         const data = resp.data;
         const $ = cheerio.load(data);
-        $("div.TableBaseWrapper:first").find('div#TableBase-1').each((_, e) => {
+        $(`div.TableBaseWrapper:${rank==1?'first':'eq(1)'}`).find(tableSelect).each((_, e) => {
             $(e).find('tbody').find('tr').each((index, team) => {
                 const rank = index + 1;
                 const teamName = $(team).find('span>a').text();
@@ -33,30 +34,34 @@ const getEasternRankings = async () => {
                 const losses = $(team).find('td.TableBase-bodyTd.TableBase-bodyTd--number').eq(1).text().trim();
                 const percentile = $(team).find('td.TableBase-bodyTd.TableBase-bodyTd--number:nth-child(3)').text().trim();
                 const pointsPerGame = $(team).find('td.TableBase-bodyTd.TableBase-bodyTd--number:nth-child(5)').text().trim();
-                easternRankings.push({ Rank: rank, Team: teamName, Wins: wins, Losses: losses, Percentile: percentile, PPG: pointsPerGame });
+                rankings.push({ Rank: rank, Team: teamName, Wins: wins, Losses: losses, Percentile: percentile, PPG: pointsPerGame });
             })
         })
     })
-    return easternRankings;
+    return rankings;
 }
+
 
 export default function NBA() {
 
     return (
         <>
-          <Text>NBA</Text>
+            <Text>NBA</Text>
+            <Box>
+            <EasternRankingsTable/>
+            <WesternRankingsTable/>
+            </Box>
         </>
     )
 }
 
-
- function EasternRankingsTable() {
+function EasternRankingsTable() {
     const [eTeams, setETeams] = useState([])
     const [rerender, setRerender] = useState(false)
     useEffect(() => {
         const getData = async () => {
             try {
-                const teamsData = await getEasternRankings();
+                const teamsData = await getEasternRankings(1);
                 setETeams(teamsData);
             } catch (error) {
                 console.error("Error fetching events:", error);
@@ -76,5 +81,31 @@ export default function NBA() {
     )
 }
 
+
+function WesternRankingsTable() {
+    const [eTeams, setETeams] = useState([])
+    const [rerender, setRerender] = useState(false)
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const teamsData = await getEasternRankings(2);
+                setETeams(teamsData);
+            } catch (error) {
+                console.error("Error fetching events:", error);
+            }
+        };
+        getData();
+        setTimeout(() => {
+            setRerender(true)
+        }, 2000)
+
+    }, []);
+
+    return (
+        <>
+            {eTeams?.length == 0 && eTeams ? <Spinners /> : <Table data={eTeams}></Table>}
+        </>
+    )
+}
 
 
